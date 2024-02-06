d5a8a05ed0f33625890c968aac7c700f2f79ebb2
João P. P. Ramos <jppramoss@gmail.com>
2024-02-06 10:24:21 -0300

    feat: Add customers route,service and controller
    Rota, Serviço e módulo de controle adicionados de teste adicionados
    à entidade "customer"

---
+++ backend/src/controllers/CreateCustomerController.ts
@@ -0,0 +1,20 @@
+import {FastifyRequest, FastifyReply} from 'fastify'
+import { CreateCustomerService } from '../services/CreateCustomerService'}
+
+class CreateCustomerController{
+    //método handle recebe dois parâmetro request/reply
+    async handle(request: FastifyRequest, reply: FastifyReply){
+        
+        //inicializa o serviço (inicializa a classe que foi criada para o serviço)
+        const customerService = new CreateCustomerService()
+
+        //Espera a execução do método 'execute()' presente na classe do serviço 
+        const customer = await customerService.execute();
+        
+        //devolve para a api o que o retorno da execução
+        reply.send(customer);
+    
+    }
+}
+
+export { CreateCustomerController }
