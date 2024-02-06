0f65f768ce7c2171345a0484817d80207e304fc1
João P. P. Ramos <jppramoss@gmail.com>
2024-02-06 10:27:14 -0300

    fix: Typo in customers controller module

--- backend/src/controllers/CreateCustomerController.ts
+++ backend/src/controllers/CreateCustomerController.ts
@@ -1,5 +1,5 @@
 import {FastifyRequest, FastifyReply} from 'fastify'
-import { CreateCustomerService } from '../services/CreateCustomerService'}
+import { CreateCustomerService } from '../services/CreateCustomerService'
 
 class CreateCustomerController{
     //método handle recebe dois parâmetro request/reply
