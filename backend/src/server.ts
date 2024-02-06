e38f616433305a99d4fcc939861e72b0cdfbad6b
João P. P. Ramos <jppramoss@gmail.com>
2024-02-06 10:05:08 -0300

    feat: Add basic prisma client config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,21 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+//importa rotas do diretório ./routes
+import { routes } from './routes';
 
+//Retorna o logger 
 const app = Fastify({ logger: true })
 
+//inicializa a aplicação 
 const start = async () => {
+    
+    await app.register(cors);
+    await app.register(routes);
 
         try{
             await app.listen({port:3333})
         }catch(err){
+        //Para a aplicação caso haja erro 
         process.exit(1);
         }
 }
