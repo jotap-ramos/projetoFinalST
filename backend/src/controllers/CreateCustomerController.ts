2312d7f258a5ddf76073ec9837c86556ff85f405
João P. P. Ramos <jppramoss@gmail.com>
2024-02-06 11:15:26 -0300

    feat: Add interface to store name, email and status into db

--- backend/src/controllers/CreateCustomerController.ts
+++ backend/src/controllers/CreateCustomerController.ts
@@ -5,11 +5,14 @@ class CreateCustomerController{
     //método handle recebe dois parâmetro request/reply
     async handle(request: FastifyRequest, reply: FastifyReply){
         
+        //guarda os valores de name e email enviados via requisição em formato JSON
+        const {name, email} = request.body as { name: string, email: string};
+
         //inicializa o serviço (inicializa a classe que foi criada para o serviço)
         const customerService = new CreateCustomerService()
 
         //Espera a execução do método 'execute()' presente na classe do serviço 
-        const customer = await customerService.execute();
+        const customer = await customerService.execute({name, email});
         
         //devolve para a api o retorno da execução do método execute()
         reply.send(customer);
--- backend/src/services/CreateCustomerService.ts
+++ backend/src/services/CreateCustomerService.ts
@@ -1,11 +1,30 @@
 import prismaClient from "../prisma";
 
+//interface/estrutura com as propriedades do usuário que vamos manipular depois
+interface CreateCustomerProps{
+    name: string;
+    email: string;
+}
+
 class CreateCustomerService{
-    async execute(){
+    async execute({name, email}: CreateCustomerProps){
 
-        console.log("ROTA FOI CHAMADA");
+        //tratativa de erro simples
+        //caso usuário não preencha nome ou email (ambos obrigatórios)
+        if(!name || !email){
+            throw new Error("Preencha todos os campos")
+        }
 
-        return { ok : true};
+        //armazena dados do usuário no banco de dados
+        const customer = await prismaClient.customer.create({
+            data: {
+                name,
+                email,
+                status: true
+            }
+        })
+
+        return customer
     }
 }
 
