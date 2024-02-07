dd23a7c04cdd944cb061e71045c3ab3e1fa9bf87
João P. P. Ramos <jppramoss@gmail.com>
2024-02-07 11:30:04 -0300

    feat: Add route, service and controller for DeleteCustomer feat

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify"
 import { CreateCustomerController } from "./controllers/CreateCustomerController"
 import { ListCustomersController } from './controllers/ListCustomersController'
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController"
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
     //Rota de teste inicial 
@@ -16,6 +17,11 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
     //Rota para listagem de clientes
     fastify.get("/customers", async (request:FastifyRequest, reply: FastifyReply) => {
         return new ListCustomersController().handle(request, reply)
+    })
+
+    //Rota para deleção de clientes
+      fastify.delete("/customer", async (request:FastifyRequest, reply: FastifyReply) => {
+        return new DeleteCustomerController().handle(request, reply)
     })
 
 }
